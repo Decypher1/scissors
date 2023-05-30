e3480c04d8b0eeb8a3d6086aa009cc642afe49ef
decypher1 <mumekwe2@gmail.com>
2023-05-30 04:14:47 +0100

    Main file updated

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ require("dotenv").config();
 const dbConnect = require('./config/db');
 
 const app = express();
+
+//connect to database
 dbConnect();
 const PORT = process.env.PORT || 4000;
+
 app.use(express.json({extended: false}));
+
+//routes
+app.use("/", require('./routes/index'));
+app.use("/api/url", require('./routes/urlRoute'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
