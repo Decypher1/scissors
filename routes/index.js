e1764c3593c6f384ed271fa3d0d222e2707ef684
decypher1 <mumekwe2@gmail.com>
2023-06-08 03:55:07 +0100

    Redirecting the short url to the long url

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const urlModel = require('../models/urlModel');
+
+// @route       GET /:code
+// @desc        Reroute the created link to the long url
+
+router.get("/:code", async (req, res) => {
+    try {
+        const url = await urlModel.findOne({urlCode: req.params.code});
+
+        if(url) {
+            return res.redirect(url.longUrl)
+        }else{
+            return res.status(404).json({message : "Not Found"})
+        }
+    } catch (err){
+        console.error(err);
+        res.status(500).json('Server error');
+    }
+
+})
 
 module.exports = router;
